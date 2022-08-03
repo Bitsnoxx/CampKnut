bfc114f62e72a318d2a2b75b2f5edc62f5b15eac
Miklós Dániel <daniel.miklos0@gmail.com>
2022-08-03 14:11:17 +0200

    example styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import PageLayout from "../components/layout/PageLayout";
+import { MDXProvider } from "@mdx-js/react";
+
+import { components } from "../components/mdxStyling";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PageLayout>
+      <MDXProvider components={components}>
+        <Component {...pageProps} />
+      </MDXProvider>
+    </PageLayout>
+  );
 }
 
 export default MyApp;
