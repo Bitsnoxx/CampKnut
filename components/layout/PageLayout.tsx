f6fbc9ad90dca381f7c50d0c672e5a0cd0375f6c
Miklós Dániel <daniel.miklos0@gmail.com>
2022-08-03 14:14:16 +0200

    fix text color

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -7,7 +7,7 @@ type PageLayoutProps = {
 
 export default function PageLayout({ children }: PageLayoutProps) {
   return (
-    <div className="flex flex-col min-h-screen bg-knut-light-bg dark:bg-knut-dark-bg">
+    <div className="flex flex-col min-h-screen text-knut-light-text dark:text-knut-dark-text bg-knut-light-bg dark:bg-knut-dark-bg">
       <Header />
       <main className="container flex-1 mx-auto max-w-5xl p-4 ">
         {children}
