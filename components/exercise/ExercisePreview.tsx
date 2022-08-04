f1d378de5ed21daca6c6382469dbee9159e26380
Miklós Dániel <daniel.miklos0@gmail.com>
2022-08-04 22:55:37 +0200

    add priority for preview

--- components/exercise/ExercisePreview.tsx
+++ components/exercise/ExercisePreview.tsx
@@ -14,7 +14,13 @@ export default function ExercisePreview({
       <a>
         <div className="h-full rounded-sm px-2 py-4 bg-knut bg-knut-light-bg-info dark:bg-knut-dark-bg-info hover:bg-knut-light-bg hover:dark:bg-knut-dark-bg transition duration-300">
           <div className="inline-block relative w-full aspect-video">
-            <Image objectFit="cover" src={imageUrl} layout="fill" alt={name} />
+            <Image
+              priority
+              objectFit="cover"
+              src={imageUrl}
+              layout="fill"
+              alt={name}
+            />
           </div>
           <h1 className="text-2xl font-bold">{name}</h1>
           <div className="my-2">
