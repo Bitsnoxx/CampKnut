ae052295fd8be0e7f59bf841ddc7ad38204ca22f
Bitsnoxx <Bitsnoxx@users.noreply.github.com>
2022-08-04 19:00:51 +0200

    Revert "Improvements (#20)" (#24)
    This reverts commit 36bb4baff6e2cec236713dd7bdbeeb49058f65ac.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Container } from "@mantine/core";
+import styles from "../styles/Home.module.css";
+import {
+  FaTwitch,
+  FaYoutube,
+  FaTwitter,
+  FaInstagram,
+  FaReddit,
+} from "react-icons/fa";
+import { Container, Center, AspectRatio } from "@mantine/core";
 import { introduction, tips } from "../content/text";
 import { MdContactSupport } from "react-icons/md";
 import Link from "next/link";
@@ -16,37 +24,31 @@ const Home: NextPage = () => {
             {introduction.title} 💪
           </p>
         </header>
+
         <div className="page-body">
           <Image
             src="/CampKnut.webp"
             alt="Camp Knut"
             width={2560}
-            height={1450}
+            height={1250}
             priority={true}
             decoding="async"
-            className="aspect-auto"
+            className="aspect-video"
           />
-          <section className="mb-10 mt-5">
-            <h1 className="text-2xl font-bold">
-              What is {introduction.title}?
-            </h1>
-            <section>
-              <h3>{introduction.whatIs}</h3>
-            </section>
-            <div className="my-10">
-              <h2 className="mt-8 text-2xl font-bold">Goal 🏆</h2>
-              <h1>{introduction.goal}</h1>
-            </div>
-            <div className="my-10">
-              <h2 className="mt-8 text-2xl font-bold">Who is Knut?</h2>
-              <h1>{introduction.whoIsKnut}</h1>
-            </div>
-          </section>
 
-          <div className="flex my-10">
+          <h2 className="mt-8 text-2xl font-bold">
+            What is {introduction.title}?
+          </h2>
+          <p>{introduction.whatIs}</p>
+          <h2 className="mt-8 text-2xl font-bold">Goal🏆</h2>
+          <p>{introduction.goal}</p>
+          <h2 className="mt-8 text-2xl font-bold">Who is Knut?</h2>
+          <p>{introduction.whoIsKnut}</p>
+
+          <div className="flex mb-8">
             <div className="w-1/2">
               <h1 className="text-2xl font-bold mt-4">Trainers</h1>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://twitch.tv/knut"
                   target="_blank"
@@ -55,7 +57,7 @@ const Home: NextPage = () => {
                   Knut
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/wakewilder"
                   target="_blank"
@@ -65,7 +67,7 @@ const Home: NextPage = () => {
                 </a>
               </p>
               <h1 className="text-2xl font-bold mt-4">Judges</h1>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://twitch.tv/knut"
                   target="_blank"
@@ -74,7 +76,7 @@ const Home: NextPage = () => {
                   Knut
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/asmongold"
                   target="_blank"
@@ -83,7 +85,7 @@ const Home: NextPage = () => {
                   Asmongold
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/wakewilder"
                   target="_blank"
@@ -92,7 +94,7 @@ const Home: NextPage = () => {
                   WakeWilder
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/malena"
                   target="_blank"
@@ -103,19 +105,19 @@ const Home: NextPage = () => {
               </p>
 
               <h1 className="text-2xl font-bold mt-4">Guests</h1>
-              <p className="text-neutral-300	hover:text-neutral-50">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/mitchjones"
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <span className="">MitchJones</span>
+                  MitchJones
                 </a>
               </p>
             </div>
             <div className="w-1/2">
               <h1 className="text-2xl font-bold mt-4">Streamers</h1>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/mizkif"
                   target="_blank"
@@ -124,7 +126,7 @@ const Home: NextPage = () => {
                   Mizkif
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/nmplol"
                   target="_blank"
@@ -133,7 +135,7 @@ const Home: NextPage = () => {
                   Nmplol
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/cyr"
                   target="_blank"
@@ -142,7 +144,7 @@ const Home: NextPage = () => {
                   Cyr
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/richwcampbell"
                   target="_blank"
@@ -151,7 +153,7 @@ const Home: NextPage = () => {
                   RichwCampbell
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/tectone"
                   target="_blank"
@@ -160,7 +162,7 @@ const Home: NextPage = () => {
                   Tectone
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/erobb221"
                   target="_blank"
@@ -169,7 +171,7 @@ const Home: NextPage = () => {
                   Erobb221
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/esfandtv"
                   target="_blank"
@@ -178,7 +180,7 @@ const Home: NextPage = () => {
                   EsfandTV
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/Lacari"
                   target="_blank"
@@ -188,7 +190,7 @@ const Home: NextPage = () => {
                 </a>
               </p>
               <h1 className="text-2xl font-bold mt-4">Chef</h1>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 <a
                   href="https://www.twitch.tv/exxzy"
                   target="_blank"
@@ -200,19 +202,31 @@ const Home: NextPage = () => {
             </div>
           </div>
 
-          <div className="my-10 font-bold">
-            <p>💡 {tips.knowYourLimits}</p>
-          </div>
-
-          <div className="my-10 font-bold">
-            <p>💡 {tips.supplements}</p>
-          </div>
+          <p></p>
+          <figure className="block-color-gray_background callout">
+            <div>
+              <span className="icon">💡</span>
+            </div>
+            <div>
+              <strong>{tips.knowYourLimits}</strong>
+            </div>
+          </figure>
+          <p></p>
+          <figure className="block-color-gray_background callout">
+            <div>
+              <span className="icon">💡</span>
+            </div>
+            <div className="mb-8">
+              <strong>{tips.supplements}</strong>
+            </div>
+          </figure>
+          <p></p>
 
           <div className="flex">
             <div className="w-1/2">
-              <h1 className="text-2xl font-bold mt-4 mb-2">🥩 Meal Plans</h1>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                {" "}
+              <h1 className="text-2xl font-bold mt-4">🥩 Meal Plans</h1>
+              <p className="underline underline-offset-4">
+                🧾{" "}
                 <a
                   href="https://docs.google.com/spreadsheets/d/1Fto6sArnx-lBoxXyFEC3cLCbVQkJdwT9-JsPD9Agra8/edit?usp=sharing"
                   target="_blank"
@@ -221,35 +235,35 @@ const Home: NextPage = () => {
                   OTK Meal plans
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                {" "}
+              <p className="underline underline-offset-4">
+                🧾{" "}
                 <a href="https://bit.ly/3vil5en">
                   Mizkif meal plan with supplements
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                {" "}
+              <p className="underline underline-offset-4">
+                🧾{" "}
                 <a href="https://bit.ly/3cReLVb">
                   Mizkif meal plan without eggs and supplements
                 </a>
               </p>
-            </div>
-
-            <div className="w-1/2">
+              <h1 className="text-2xl font-bold mt-4">💊 Supplements</h1>
+              <figure className="link-to-page underline underline-offset-4">
+                <a href="Camp%20Knut%20%F0%9F%92%AA%2017e230f0c0994684b7fd08ded2966fcf/Camp%20Knut%20Amazon%20Suplements%20by%20WakeWilder%200e73dd9909e84c338a704638a837289d.html">
+                  <span className="icon">📝</span>Camp Knut Amazon Supplements
+                  by WakeWilder
+                </a>
+              </figure>
               <h1 className="text-2xl font-bold mt-4">
                 <strong>💖</strong> Community
               </h1>
-              <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                <a href="">
-                  Amazon Supplements by <strong>WakeWilder</strong>
+              <figure className="link-to-page underline underline-offset-4">
+                <a href="Camp%20Knut%20%F0%9F%92%AA%2017e230f0c0994684b7fd08ded2966fcf/Stream%20workout%20log%20-%20Maintained%20by%20DrStone%209b89ac122f0749328c7f13ffc63fe256.html">
+                  <span className="icon">📝</span>Stream workout log -
+                  Maintained by DrStone
                 </a>
               </figure>
-              <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                <a href="">
-                  Stream workout log - Maintained by <strong>DrStone</strong>
-                </a>
-              </figure>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <p className="underline underline-offset-4">
                 📀{" "}
                 <a
                   href="https://open.spotify.com/playlist/0bNUdcAFj5Un6XNKZ5Xora?si=e5e3832466244996"
@@ -263,8 +277,6 @@ const Home: NextPage = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <strong>Tahtooz</strong>
-
                   <strong>
                     <strong>Tahtooz</strong>
                   </strong>
@@ -281,24 +293,8 @@ const Home: NextPage = () => {
                 </a>
               </p>
             </div>
-          </div>
 
-          <div className="flex">
             <div className="w-1/2">
-              <h1 className="text-2xl font-bold mt-4 mb-2">📕 Plans</h1>
-              <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                <a href="">Workout Beginner</a>
-              </figure>
-              <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                <a href="">Workout plan Week 1</a>
-              </figure>
-              <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                <a href="">Workout plan Week 2</a>
-              </figure>
-              <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                <Link href="/exercises">
-                  <a>Exercises</a>
-                </Link>
               <h1 className="text-2xl font-bold mt-4">📕 Plans</h1>
               <figure className="link-to-page underline underline-offset-4">
                 <Link href={"/workout/beginner"} passHref>
@@ -326,16 +322,18 @@ const Home: NextPage = () => {
                   <span className="icon">📅</span>Camp Knut - Calendar
                 </a>
               </figure>
-            </div>
-            <div className="w-1/2">
-              <h1 className="text-2xl font-bold mt-4 mb-2">🛠️ Tools</h1>
-              <p className="text-neutral-300">
-                MyFitnessPal{" "}
+              <figure className="link-to-page underline underline-offset-4">
+                <a href="Camp%20Knut%20%F0%9F%92%AA%2017e230f0c0994684b7fd08ded2966fcf/Weekly%20results%200756d199cefd4d90b2be7aad2faf8f0d.html">
+                  <span className="icon">📝</span>Weekly results
+                </a>
+              </figure>
+              <h1 className="text-2xl font-bold mt-4">🛠️ Tools</h1>
+              <p className="underline underline-offset-4">
+                📱 MyFitnessPal{" "}
                 <a
                   href="https://apps.apple.com/us/app/myfitnesspal-calorie-counter/id341232718"
                   target="_blank"
                   rel="noreferrer"
-                  className="text-neutral-300 hover:text-neutral-50 hover:font-bold hover:font-bold"
                 >
                   iOS{" "}
                 </a>
@@ -344,13 +342,12 @@ const Home: NextPage = () => {
                   href="https://play.google.com/store/apps/details?id=com.myfitnesspal.android&amp;hl=en&amp;gl=US"
                   target="_blank"
                   rel="noreferrer"
-                  className="text-neutral-300 hover:text-neutral-50 hover:font-bold"
                 >
                   Android
                 </a>
               </p>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                {" "}
+              <p className="underline underline-offset-4">
+                📱{" "}
                 <a
                   href="https://www.bodybuilding.com/fun/bmr_calculator.htm"
                   target="_blank"
@@ -362,44 +359,22 @@ const Home: NextPage = () => {
             </div>
           </div>
 
-          <div className="flex">
+          <div className="flex mt-20">
             <div className="w-1/2">
-              <h1 className="text-2xl font-bold mt-4 mb-2">📈 Progress</h1>
-              <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                <a href="">
-                  <h1> Camp Knut - Calendar</h1>
+              <h3 className="text-xl font-bold mt-4">Want to help out?</h3>
+              <figure className="link-to-page underline underline-offset-4">
+                <a href="Camp%20Knut%20%F0%9F%92%AA%2017e230f0c0994684b7fd08ded2966fcf/Roadmap%20ffe306ad089245cc99038389d2d9061d.html">
+                  <span className="icon">🚗</span>Roadmap
                 </a>
               </figure>
-
-              <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                <a href="">Weekly results</a>
-              </figure>
             </div>
-            <div className="w-1/2">
-              <h1 className="text-2xl font-bold mt-4 mb-2">📃 Articles</h1>
-              <p className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
-                {" "}
-                <a href="https://physiqonomics.com/fat-loss/">
-                  The Best Fat Loss Article on the Motherfuckin’ Internet
-                </a>
-              </p>
-            </div>
-          </div>
-
-          <div className="text-center my-20">
-            <h3 className="text-xl font-bold mt-4">Want to help out?</h3>
-            <figure className="link-to-page text-neutral-300 hover:text-neutral-50 hover:font-bold">
-              <a href="https://github.com/Bitsnoxx/CampKnut/issues">
-                <h1>Roadmap</h1>
-              </a>
-            </figure>
           </div>
 
           <figure className="block-color-gray_background callout mt-8">
             <div className="text-center">
               If you have questions about training - Post them in #camp-knut in
               the{" "}
-              <strong className="text-neutral-300 hover:text-neutral-50 hover:font-bold">
+              <strong className="underline underline-offset-4">
                 <a
                   href="http://discord.gg/knut"
                   target="_blank"
