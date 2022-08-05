13c7ff0347b4d98ce33a0be515447d011f6020cd
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2022-08-05 15:19:12 +0200

    supplements content (#47)

--- components/ui/Important.tsx
+++ components/ui/Important.tsx
@@ -1,5 +1,5 @@
 export default function Important(props: any) {
-  const text = props.children.props.children;
+  const text = props.children ?? props.children.props.children;
   return (
     <div className="p-4 my-4 bg-knut-light-bg-info dark:bg-knut-dark-bg-info">
       {text}
