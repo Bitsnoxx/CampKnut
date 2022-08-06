9f86de1d31767b768a06c059f856fb6d03ff37e2
Miklós Dániel <daniel.miklos0@gmail.com>
2022-08-06 13:15:31 +0200

    fix html

--- components/mainPage/Introduction.tsx
+++ components/mainPage/Introduction.tsx
@@ -2,25 +2,26 @@ import { introduction } from "./../../content/text";
 
 export default function Introduction() {
   return (
-    <section className="mb-10 mt-5">
-      <h1 className="text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
-        What is {introduction.title}?
-      </h1>
-      <section>
-        <h3>{introduction.whatIs}</h3>
-      </section>
-      <div className="my-10">
-        <h2 className="mt-8 text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
-          Goal 🏆
+    <>
+      <h1 className="text-3xl font-bold">Camp Knut</h1>
+      <div className="mb-10 mt-5">
+        <h2 className="text-center text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
+          What is Camp Knut?
         </h2>
-        <h1>{introduction.goal}</h1>
+        <p>{introduction.whatIs}</p>
+        <section className="my-10">
+          <h2 className="mt-8 text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
+            Goal 🏆
+          </h2>
+          <p>{introduction.goal}</p>
+        </section>
+        <section className="my-10">
+          <h2 className="mt-8 text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
+            Who is Knut?
+          </h2>
+          <p>{introduction.whoIsKnut}</p>
+        </section>
       </div>
-      <div className="my-10">
-        <h2 className="mt-8 text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
-          Who is Knut?
-        </h2>
-        <h1>{introduction.whoIsKnut}</h1>
-      </div>
-    </section>
+    </>
   );
 }
