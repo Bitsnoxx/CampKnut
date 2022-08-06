7b867ef6dcc06db3da69050de10fa2f34c7ffba5
Miklós Dániel <daniel.miklos0@gmail.com>
2022-08-06 14:54:07 +0200

    add Title comp

---
+++ components/ui/Title.tsx
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import React from "react";
+
+const headingPossibleValues = [1, 2, 3, 4, 5, 6] as const;
+
+type HeadingType = typeof headingPossibleValues[number];
+
+type TitleProps = {
+  children: string;
+  heading: HeadingType;
+  className?: string;
+};
+
+const baseStyles = {
+  mutual: "light:text-knut-light-header dark:text-knut-dark-header",
+  1: "text-3xl font-black",
+  2: "text-2xl font-bold",
+  3: "text-xl font-semibold",
+  4: "text-lg font-medium",
+  5: "text-base font-normal",
+  6: "text-sm font-light",
+};
+
+export default function Title({ children, heading, className }: TitleProps) {
+  if (!headingPossibleValues.includes(heading)) return null;
+
+  const style = clsx(className, baseStyles.mutual, baseStyles[heading]);
+
+  const component = React.createElement(
+    `h${heading}`,
+    { className: style },
+    children
+  );
+
+  return component;
+}
