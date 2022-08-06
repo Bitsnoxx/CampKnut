c177702da5ed677927d13771ffa19a0dad008dd5
Miklós Dániel <daniel.miklos0@gmail.com>
2022-08-06 13:52:23 +0200

    consistent sections

--- components/mainPage/Introduction.tsx
+++ components/mainPage/Introduction.tsx
@@ -3,25 +3,39 @@ import { introduction } from "./../../content/text";
 export default function Introduction() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Camp Knut</h1>
-      <div className="mb-10 mt-5">
-        <h2 className="text-center text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
-          What is Camp Knut?
-        </h2>
-        <p>{introduction.whatIs}</p>
-        <section className="my-10">
-          <h2 className="mt-8 text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
+      <h1 className="text-3xl font-bold text-center my-16 text-knut-light-header dark:text-knut-dark-header">
+        Camp Knut
+      </h1>
+      <div className="my-5">
+        <IntroductionSection>
+          <h2 className="text-2xl font-bold text-knut-light-header dark:text-knut-dark-header">
+            What is Camp Knut?
+          </h2>
+          <p>{introduction.whatIs}</p>
+        </IntroductionSection>
+
+        <IntroductionSection>
+          <h2 className="mt-8 text-2xl font-bold text-knut-light-header dark:text-knut-dark-header">
             Goal 🏆
           </h2>
           <p>{introduction.goal}</p>
-        </section>
-        <section className="my-10">
-          <h2 className="mt-8 text-2xl font-bold light:text-knut-light-header dark:text-knut-dark-header">
+        </IntroductionSection>
+
+        <IntroductionSection>
+          <h2 className="mt-8 text-2xl font-bold text-knut-light-header dark:text-knut-dark-header">
             Who is Knut?
           </h2>
           <p>{introduction.whoIsKnut}</p>
-        </section>
+        </IntroductionSection>
       </div>
     </>
   );
 }
+
+const IntroductionSection = ({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) => {
+  return <section className="my-10">{children}</section>;
+};
