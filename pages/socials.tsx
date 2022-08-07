82c80f27b0e436e39a8f436e81b6d96891d4e105
RealIndrit <->
2022-08-07 22:08:20 +0200

    Hej, Hej, Monika, Hej på dig Monika.

--- pages/socials.tsx
+++ pages/socials.tsx
@@ -1,5 +1,5 @@
 import PageLayout from "../components/layout/PageLayout";
-import { socials } from "./../content/links";
+import { socials } from "../content/links";
 
 export default function SocialsPage() {
   return (
@@ -9,7 +9,7 @@ export default function SocialsPage() {
         {socials.map((e) => {
           return (
             <a key={e.url} href={e.url} target="_blank" rel="noreferrer">
-              <div className="p-4 bg-knut-light-bg-info dark:bg-knut-dark-bg-info">
+              <div className="p-4 bg-knut-light-bg-info dark:bg-knut-dark-bg-info hover:dark:bg-knut-dark-bg hover:bg-knut-light-bg">
                 <h1 className="text-xl">{e.name}</h1>
                 <span className="font-light">
                   {e.url.replace("https://", "").replace("www.", "")}
--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -4,13 +4,13 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_KEY
 );
-export const loginTwitch = async () => {
+export const signInTwitch = async () => {
   await supabase.auth.signIn({
     provider: "twitch",
   });
 };
 
-export const signOut = async () => {
+export const signOutTwitch = async () => {
   await supabase.auth.signOut();
 };
 
