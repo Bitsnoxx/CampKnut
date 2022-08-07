7c29332eab88e545dfd1293b36d582abc142d362
Gisli Nielsen <Gislionielsen@gmail.com>
2022-08-07 20:48:57 +0200

    added hall of fame (#77)
    Co-authored-by: General Knowledge <devo@fangroup.io>

---
+++ content/contributors.ts
@@ -0,0 +1,21 @@
+type ContributorType = {
+	username: string,
+	help: string,
+};
+
+export const contributors: ContributorType[] = [
+	{ username: 'DrStone', help: 'Workout Log' },
+	{ username: 'Tahtooz', help: 'Spotify playlist, design' },
+	{ username: 'WaYa', help: 'Tip' },
+	{ username: 'Shatty', help: 'Feedback' },
+	{ username: 'JigaPlank', help: 'Exercises, design' },
+	{ username: 'Coleslaw', help: 'Exercises' },
+	{ username: 'xxtransience', help: 'Exercises' },
+	{ username: 'Ivarsen', help: 'Exercises' },
+	{ username: 'tyler718', help: 'Exercises' },
+	{ username: 'trumanblack', help: 'Fix' },
+	{ username: 'Vizumn', help: 'Fix' },
+	{ username: 'Hubert', help: 'Fix' },
+	{ username: 'Bakalaka', help: 'Design' },
+	{ username: 'Aakara', help: 'Development' },
+]
