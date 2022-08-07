48c6c60c18eb8e1468f59f998172dcc0588f60cd
Bitsnoxx <bitsnoxx@gmail.com>
2022-08-07 18:30:10 +0200

    fix
    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MDXProvider } from "@mdx-js/react";
 import { ThemeProvider } from "next-themes";
+import { supabase } from "../utils/supabaseClient";
 
 import { components } from "../components/mdxStyling";
 
