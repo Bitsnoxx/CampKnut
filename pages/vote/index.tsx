61325767b62f125e3c44c8cbb80632e3b371cf5e
Jeremy Sweazy <jswaze@gmail.com>
2022-08-08 00:23:36 -0400

    Add voting functionality in supabase

--- pages/vote/index.tsx
+++ pages/vote/index.tsx
@@ -1,101 +1,226 @@
+import React, { useState, useEffect, useCallback } from "react";
 import Image from "next/image";
-import { participants } from "../../content/streamers";
-import React from "react";
-import PageLayout from "../../components/layout/PageLayout";
-import {supabase, signInTwitch, signOutTwitch} from "../../utils/supabaseClient";
-const session = supabase.auth.session();
+import { User } from "@supabase/supabase-js";
+import clsx from "clsx";
+
+import { participants } from "content/streamers";
+import PageLayout from "components/layout/PageLayout";
+import { supabase, signInTwitch, signOutTwitch } from "utils/supabaseClient";
+
+const streamers = participants.find((e) => e.category === "streamers")?.members;
 
 export default function Vote() {
-  const streamers = participants.find((e) => e.category === "streamers");
-  const session = supabase.auth.session();
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+  const [vote, setVote] = useState<string>();
+  const [previousVote, setPreviousVote] = useState<string>();
+  const [hasVoted, setHasVoted] = useState(false);
+  const [showVoteError, setShowVoteError] = useState(false);
+
+  const submitVote = useCallback(async () => {
+    setPreviousVote(vote);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    if (hasVoted) {
+      const { data, error } = await supabase
+        .from("votes")
+        .update({ vote: vote })
+        .eq("user_id", user?.id);
+
+      // TODO: handle errors
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from("votes")
+      .insert({ user_id: user?.id, vote: vote }, { returning: "minimal" });
+
+    // TODO: handle errors
+    setHasVoted(true);
+  }, [vote, user, hasVoted]);
+
+  const hasUserVoted = useCallback(async (userId: User["id"]) => {
+    const { data, error } = await supabase
+      .from("votes")
+      .select("vote")
+      .filter("user_id", "eq", userId);
+
+    // TODO: handle errors
+    setHasVoted(!!data?.length);
+    if (data?.length) {
+      setVote(data[0].vote);
+      setPreviousVote(data[0].vote);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const session = supabase.auth.session();
+    const user = session?.user ?? null;
+    setUser(user);
+
+    if (user) {
+      hasUserVoted(user.id);
+    }
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        setUser(session?.user ?? null);
+      }
+    );
+
+    return () => {
+      listener?.unsubscribe();
+    };
+  }, []);
+
+  if (isLoading) {
+    return (
+      <PageLayout>
+        <div role="status" className="w-14 h-14 mx-auto">
+          <svg
+            aria-hidden="true"
+            className="mr-2 w-full h-full text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </PageLayout>
+    );
+  }
+
+  if (!user) {
+    return (
+      <PageLayout>
+        <article>
+          <h1 className="text-3xl mb-4 font-bold mt-5 text-center mr-4 light:text-knut-light-header dark:text-knut-dark-header">
+            {" "}
+            You need to login to vote
+          </h1>
+
+          <section className="hero container mx-auto flex justify-center">
+            <Image
+              src={`/HUHH.webp`}
+              className="mx-auto"
+              height={128}
+              width={128}
+              alt="HUHH"
+            ></Image>
+          </section>
+
+          <div className="text-3xl mb-4 font-bold mt-5 text-center mr-4">
+            <button
+              onClick={() => signInTwitch()}
+              className="text-2xl text-center bg:bg-other-twitch rounded-xl py-2.5 px-2.5 bg-knut-other-twitch text-knut-dark-header"
+            >
+              Sign in
+            </button>
+          </div>
+        </article>
+      </PageLayout>
+    );
+  }
 
   return (
     <PageLayout>
       <article>
-        {session ? (
-          <>
-            <h1 className="text-3xl mb-4 mt-5 text-center mr-4 font-black light:text-knut-light-header dark:text-knut-dark-header">
-              Vote on your favourite streamer{" "}
-              {session?.user?.user_metadata.nickname}
-              <span className="pl-4">
-                <Image
-                  src={session?.user?.user_metadata.picture}
-                  alt="Camp Knut"
-                  width={64}
-                  height={64}
-                  priority={true}
-                  decoding="async"
-                  className="aspect-auto"
-                ></Image>
-              </span>
-            </h1>
-              <div className="text-3xl mb-4 font-bold mt-5 text-center mr-4">
-                  <button
-                      onClick={() => signOutTwitch()}
-                      className="text-2xl text-center bg:bg-other-twitch rounded-xl py-2.5 px-2.5 bg-knut-other-twitch text-knut-dark-header"
-                  >
-                      Sign Out
-                  </button>
-              </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-              {streamers?.members.map((e) => (
-                <div
-                  key={e.name}
-                  className="cursor-pointer rounded-xl p-3 bg-knut-light-bg-info dark:bg-knut-dark-bg-info hover:bg-knut-light-bg hover:dark:bg-knut-dark-bg transition duration-300"
-                >
-                  <div
-                    key={e.name}
-                    className=" overflow-hidden rounded-xl bg-knut-light-bg-info dark:bg-knut-dark-bg-info hover:bg-knut-light-bg hover:dark:bg-knut-dark-bg transition duration-300"
-                  >
-                    <span
-                      key={e.name}
-                      className="text-center text-2xl font-black light:text-knut-light-header dark:text-knut-dark-header"
-                    >
-                      <h3 className="pb-2">{e.name}</h3>
-                      <section className="hero container mx-auto flex justify-center">
-                        <Image
-                          src={`/participants/${e.twitchName}.webp`}
-                          className="mx-auto rounded-md"
-                          height={250}
-                          width={250}
-                          alt={e.name}
-                        ></Image>
-                      </section>
-                    </span>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </>
-        ) : (
-          <>
-            <div>
-              <h1 className="text-3xl mb-4 font-bold mt-5 text-center mr-4 light:text-knut-light-header dark:text-knut-dark-header">
-                {" "}
-                You need to login to vote
-              </h1>
-
-              <section className="hero container mx-auto flex justify-center">
-                <Image
-                  src={`/HUHH.webp`}
-                  className="mx-auto"
-                  height={128}
-                  width={128}
-                  alt="HUHH"
-                ></Image>
-              </section>
-
-              <div className="text-3xl mb-4 font-bold mt-5 text-center mr-4">
-                <button
-                  onClick={() => signInTwitch()}
-                  className="text-2xl text-center bg:bg-other-twitch rounded-xl py-2.5 px-2.5 bg-knut-other-twitch text-knut-dark-header"
-                >
-                  Sign in
-                </button>
-              </div>
-            </div>
-          </>
+        <h1 className="text-3xl text-center mb-4 mt-5 mr-4 font-black light:text-knut-light-header dark:text-knut-dark-header">
+          Vote on your favourite streamer{" "}
+          <span className="inline-flex justify-center items-center">
+            {user?.user_metadata.nickname}
+            <Image
+              src={user?.user_metadata.picture}
+              alt="Camp Knut"
+              width={25}
+              height={25}
+              priority={true}
+              decoding="async"
+              className="aspect-auto inline-block pl-3"
+            />
+          </span>
+        </h1>
+        {hasVoted && (
+          <h2 className="text-center">
+            You have voted for{" "}
+            <strong>
+              <em>
+                {streamers?.find((s) => s.twitchName === previousVote)?.name}
+              </em>
+            </strong>
+            . You can change your vote below.
+          </h2>
         )}
+        <div className="text-3xl mb-4 font-bold mt-5 text-center mr-4">
+          <button
+            onClick={() => signOutTwitch()}
+            className="text-2xl text-center bg:bg-other-twitch rounded-xl py-2.5 px-2.5 bg-knut-other-twitch text-knut-dark-header"
+          >
+            Sign Out
+          </button>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+          {streamers?.map(({ twitchName, name }) => {
+            const selected = vote === twitchName;
+            return (
+              <button
+                type="button"
+                key={twitchName}
+                onClick={() => setVote(twitchName)}
+                className={clsx("rounded-xl p-3  transition duration-300", {
+                  ["bg-knut-light-bg-info dark:bg-knut-dark-bg-info"]:
+                    !selected,
+                  ["bg-knut-dark-bg-info dark:bg-knut-light-bg-info"]: selected,
+                })}
+              >
+                <h3
+                  className={clsx("pb-2 text-2xl font-black", {
+                    ["text-black dark:text-white"]: !selected,
+                    ["text-white dark:text-black"]: selected,
+                  })}
+                >
+                  {name}
+                </h3>
+                <section className="hero container mx-auto flex justify-center">
+                  <Image
+                    src={`/participants/${twitchName}.webp`}
+                    className="mx-auto rounded-md"
+                    height={250}
+                    width={250}
+                    alt={name}
+                  ></Image>
+                </section>
+              </button>
+            );
+          })}
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            type="button"
+            onClick={submitVote}
+            className={clsx(" font-bold py-3 px-6 rounded", {
+              "bg-gray-300 text-gray-500 cursor-not-allowed": !!!vote,
+              "bg-knut-dark-tag text-white": !!vote,
+            })}
+            disabled={!!!vote}
+          >
+            Vote
+          </button>
+        </div>
       </article>
     </PageLayout>
   );
--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,13 +1,20 @@
 import { createClient } from "@supabase/supabase-js";
 
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_KEY
 );
-export const signInTwitch = async () => {
-  await supabase.auth.signIn({
-    provider: "twitch",
-  });
+export const signInTwitch = async (redirectTo = `${baseUrl}/vote`) => {
+  await supabase.auth.signIn(
+    {
+      provider: "twitch",
+    },
+    { redirectTo }
+  );
 };
 
 export const signOutTwitch = async () => {
