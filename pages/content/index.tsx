015f5a725db4546fb99f396416d2b28e6aea20bf
Bitsnoxx <Bitsnoxx@users.noreply.github.com>
2022-08-11 00:49:15 +0200

    Update index.tsx

--- pages/content/index.tsx
+++ pages/content/index.tsx
@@ -21,19 +21,21 @@ export async function getStaticProps() {
 export default function ContentPage({ playlist }: { playlist: PlaylistItem[] }) {
   return (
     <PageLayout widthClassName="container flex-1 mx-auto p-4 max-w-none">
-      <div className="mx-auto w-11/12">
-        <h1 className="pb-4 font-bold">Content</h1>
+      <div className="w-11/12 mx-auto">
+        <h1 className="text-3xl font-bold pb-5">Content</h1>
         <div
-          className="grid auto-rows-max gap-6"
+          className="grid auto-rows-max gap-4"
           style={{
-            gridTemplateColumns: 'repeat(auto-fill,minmax(320px,1fr))',
+            gridTemplateColumns: "repeat(auto-fill,minmax(260px,1fr))",
           }}
         >
           {playlist.reverse().map((item) => {
             const { id, snippet } = item;
+            const { title, thumbnails } = snippet;
+            const { medium } = thumbnails;
 
             return (
-              <iframe
+              <a
                 key={id}
                 href={`https://www.youtube.com/watch?v=KWLDgKV0pEs${snippet.resourceId.videoId}`}
                 target="_blank"
@@ -41,17 +43,17 @@ export default function ContentPage({ playlist }: { playlist: PlaylistItem[] }) 
               >
                 <div
                   key={id}
-                  className="bg-knut h-full rounded-lg bg-knut-light-bg-info transition duration-300 hover:bg-knut-light-bg dark:bg-knut-dark-bg-info hover:dark:bg-knut-dark-bg "
+                  className="h-full bg-knut rounded-lg bg-knut-light-bg-info dark:bg-knut-dark-bg-info hover:bg-knut-light-bg hover:dark:bg-knut-dark-bg transition duration-300 "
                 >
                   <Image
                     src={medium.url}
                     height={300}
                     width={500}
                     alt="Pag"
-                    className="relative inline-block aspect-video w-full cursor-pointer rounded-xl"
+                    className="inline-block relative w-full aspect-video rounded-xl cursor-pointer"
                     priority
                   />
-                  <span className="text-l ml-2 mt-4 text-knut-light-header dark:text-knut-dark-header">
+                  <span className="ml-2 text-l mt-4 text-knut-light-header dark:text-knut-dark-header">
                     {title}
                   </span>
                 </div>
